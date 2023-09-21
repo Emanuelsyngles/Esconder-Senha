db9617edde5d1258536b237d970df561f5238638
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-09-21 18:15:19 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+//aparecer conteúdo oculto
 function aparecer() {
     let inputPass = document.getElementById('senha')
     let btn = document.getElementById('btn-senha')
 
     if(inputPass.type === 'password') {
-        inputPass.setAttribute('text')
+        inputPass.setAttribute('type', 'text')
+
+        btn.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+    }
+//Colocar novamente em modo oculto
+    else {
+        inputPass.setAttribute('type', 'password')
+
+        btn.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
     }
 }
